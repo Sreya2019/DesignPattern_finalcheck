d0b64aeed9547288f1e77a8b555421f6d1a7e31b
Sreya2019 <64250590+Sreya2019@users.noreply.github.com>
2020-05-11 16:38:23 +0530

    Update Main.java

--- dpfinalcheck-master/DesignPrincipleFinalCheck/src/Observation/Main.java
+++ dpfinalcheck-master/DesignPrincipleFinalCheck/src/Observation/Main.java
@@ -4,16 +4,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Observer puru=new Admin("Shreya");
-		Observer dc=new Admin("Saima");
-		Observer rahul=new Admin("Akshita");
+		Observer shr=new Admin("Shreya");
+		Observer sam=new Admin("Saima");
+		Observer aks=new Admin("Akshita");
 		
 		INotificationService notificationService=new NotificationService();
-		notificationService.addSubscriber(shreyaa);
+		notificationService.addSubscriber(shr);
 		notificationService.addSubscriber(sam);
-		notificationService.addSubscriber(Akshita);
+		notificationService.addSubscriber(aks);
 		notificationService.notifySubscriber();
-		notificationService.removeSubscriber(shreyaa);
+		notificationService.removeSubscriber(shr);
 		
 		
 
